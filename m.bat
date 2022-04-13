03f50f3769b26968011040da9fe30ee9ee574280
zamanrahimi <zaman.rahimi1368@gmail.com>
2022-04-14 01:30:06 +0500

    , - ,The file test1.py has been committed

---
+++ m.bat
@@ -0,0 +1,9 @@
+git add . 
+@echo off
+
+set /A c+=1
+
+git commit -m ""," - ","The file test1.py has been committed"
+git push 
+
+
--- test1.py
+++
@@ -1,1 +0,0 @@
-10 print('This is a Python file for testing pupose')
